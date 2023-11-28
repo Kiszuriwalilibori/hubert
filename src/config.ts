82c1238939a8c6a0ed420e4ef5552a12a08949fb
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-11-28 13:27:16 +0100

    https versoion

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const URL_EVENTS = "http://18.212.75.172:8080/api/events/";
-export const URL_CATEGORIES = "http://18.212.75.172:8080/api/categories/";
+export const URL_EVENTS = "https://18.212.75.172:8080/api/events/";
+export const URL_CATEGORIES = "https://18.212.75.172:8080/api/categories/";
